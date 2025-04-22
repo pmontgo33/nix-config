b08f10cd13e0d9fab6a0c3580a9d3d67e002719a
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-04-21 20:16:44 -0400

    added hp-nixos key to secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,9 +3,10 @@ let
   # users = [ user1 ];
 
   plasma-vm-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXXXLRz5H2YLUGkvx6KTd5h/xTv1luy/51YPkank4LG";
-  systems = [ plasma-vm-nixos ];
+  hp-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEr9aBBJ73I/tXOT00krxHglmAqZ0A8xt7Hk5s2zMwCo";
+  keys = [ plasma-vm-nixos hp-nixos ];
 in
 {
-  "secret1.age".publicKeys = [ plasma-vm-nixos ];
+  "secret1.age".publicKeys = [ keys ];
   # "secret1.age".publicKeys = [ users systems ];
 }
