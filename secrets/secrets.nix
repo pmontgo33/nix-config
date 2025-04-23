a98ddd1996240462f3c825d0c197cd3cabbe1be9
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-04-22 20:34:43 -0400

    setup agenix keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,12 +4,16 @@ let
 
   #TODO Add users and systems. Figure out how to make the lists work.
 
-  plasma-vm-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXXXLRz5H2YLUGkvx6KTd5h/xTv1luy/51YPkank4LG";
+  plasma-vm-nixos_patrick = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXXXLRz5H2YLUGkvx6KTd5h/xTv1luy/51YPkank4LG";
   hp-nixos_patrick = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEr9aBBJ73I/tXOT00krxHglmAqZ0A8xt7Hk5s2zMwCo";
+  users = [ plasma-vm-nixos_patrick hp-nixos_patrick ];
+
+  plasma-vm-nixos_system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvLDQ46pJQTzxM9/nU6GMO7EsB9LCdZEELl4YY0F/4Y";
   hp-nixos_system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZIPGwINVdrqVoIzupSTMJFOty431KipytXKaKRFdHT";
-  keys = [ plasma-vm-nixos hp-nixos_patrick hp-nixos_system ];
+  systems = [ plasma-vm-nixos_system hp-nixos_system ];
+
 in
 {
-  "tailscale_auth_key.age".publicKeys = [ plasma-vm-nixos hp-nixos_patrick hp-nixos_system ];
+  "tailscale_auth_key.age".publicKeys = users ++ systems;
   # "secret1.age".publicKeys = [ users systems ];
 }
