ada07763b319e4454ab6f4da5907d1312948294d
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-06-13 23:04:33 -0400

    new systemd remount services work to ensure mounts are up after login

--- hosts/common/extra-services/mount_home_media.nix
+++ hosts/common/extra-services/mount_home_media.nix
@@ -10,9 +10,20 @@ in {
     fileSystems."/mnt/home_media" = {
       device = "truenas:/mnt/HDD-Mirror-01/home_media";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     # optional, but ensures rpc-statsd is running for on demand mounting
     boot.supportedFilesystems = [ "nfs" ];
 
+    systemd.services."remount-home_media" = {
+      description = "Remount NFS share after network is up";
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "/run/current-system/sw/bin/mount /mnt/home_media";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
   };
 }
--- hosts/common/extra-services/mount_media.nix
+++ hosts/common/extra-services/mount_media.nix
@@ -10,9 +10,21 @@ in {
     fileSystems."/mnt/media" = {
       device = "truenas:/mnt/HDD-Mirror-01/media";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     # optional, but ensures rpc-statsd is running for on demand mounting
-    #boot.supportedFilesystems = [ "nfs" ];
+    boot.supportedFilesystems = [ "nfs" ];
+
+    systemd.services."remount-media" = {
+      description = "Remount NFS share after network is up";
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "/run/current-system/sw/bin/mount /mnt/media";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
 
   };
 }
