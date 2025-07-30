0520c698e82e7c4601cd3b59d10bbe8efbc3200e
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-07-29 20:29:59 -0400

    added lxc-base to flake

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
 
     };
 
-    ## omnitools ##
+    ## lxc-base ##
     nixosConfigurations.lxc-base = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
