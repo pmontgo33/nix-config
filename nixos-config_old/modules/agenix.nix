12455ccaec94ce446131b3489b74591f894d9f57
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-09-17 22:08:36 -0400

    cleaned up old files

--- nixos-config_old/modules/agenix.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }: 
-{
-  # imports = [ "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz"}/modules/age.nix" ];
-
-  environment.systemPackages = [ (pkgs.callPackage "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz"}/pkgs/agenix.nix" {}) ];
-}
