fb3b74be26bf852a549069f75b5f4f41d590b898
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-09-29 20:29:20 -0400

    added endurian host to flake

--- flake.nix
+++ flake.nix
@@ -165,6 +165,28 @@
 
     };
 
+    ## endurian ##
+    nixosConfigurations.endurian = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/endurian
+        sops-nix.nixosModules.sops
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = { inherit inputs; };
+
+   #       home-manager.users.patrick = import ./users/patrick/home.nix;
+
+          home-manager.sharedModules = [ sops-nix.homeManagerModules.sops ];
+        }
+      ];
+    };
+
     ## omnitools ##
     nixosConfigurations.omnitools = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
