4edd68fda912ff8a25536f20f7f173b910f013fa
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-10-02 21:18:48 -0400

    initial add grist

--- flake.nix
+++ flake.nix
@@ -187,6 +187,28 @@
       ];
     };
 
+    ## grist ##
+    nixosConfigurations.grist = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/grist
+        sops-nix.nixosModules.sops
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = { inherit inputs; };
+
+   #       home-manager.users.patrick = import ./users/patrick/home.nix;
+
+          home-manager.sharedModules = [ sops-nix.homeManagerModules.sops ];
+        }
+      ];
+    };
+
     ## omnitools ##
     nixosConfigurations.omnitools = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
