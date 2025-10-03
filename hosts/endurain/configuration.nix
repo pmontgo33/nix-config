3d745be8139a8da6daef9e7cd54b6b73235c85f7
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-10-03 14:08:39 -0400

    troubleshooting local ip issue in tailscale config

--- hosts/endurain/configuration.nix
+++ hosts/endurain/configuration.nix
@@ -98,7 +98,8 @@
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 8080 ];
-
+  # networking.firewall.allowedTCPPorts = [ 8080 ];
+  networking.firewall.enable = false;
+  
   system.stateVersion = "25.05";
 }
