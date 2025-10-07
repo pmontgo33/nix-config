4aa1fcc3e538f250b79acf3ef4986ce3b002eb0a
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-10-07 19:39:35 -0400

    added immich host

--- flake.nix
+++ flake.nix
@@ -165,6 +165,25 @@
 
     };
 
+    ## immich ##
+    nixosConfigurations.immich = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/immich
+        sops-nix.nixosModules.sops
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.sharedModules = [ sops-nix.homeManagerModules.sops ];
+        }
+      ];
+    };
+
     ## endurain ##
     nixosConfigurations.endurain = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
