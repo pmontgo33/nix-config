1db9d031620bee1848025e359ccf88b39e777cd7
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-10-07 21:00:53 -0400

    troubleshoot immich host

--- hosts/immich/configuration.nix
+++ hosts/immich/configuration.nix
@@ -15,12 +15,12 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # extra-services.mount_home_media.enable = true;
+  extra-services.mount_home_media.enable = true;
 
   # Create necessary directories
   systemd.tmpfiles.rules = [
-    "d /mnt/home_media 0755 root root -"
-    "d /mnt/home_media/immich 0750 immich immich -"
+    "d /mnt/home_media 0755 immich immich -"
+    # "d /mnt/home_media/immich 0750 immich immich -"
   ];
 
   # Immich service configuration
