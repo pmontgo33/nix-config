faf53a3d84b997cc507500f4d8b624fd27c2e9b5
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-10-25 22:24:20 -0400

    added bifrost config without modification

--- flake.nix
+++ flake.nix
@@ -165,13 +165,32 @@
 
     };
 
+    ## bifrost ##
+    nixosConfigurations.bifrost = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/bifrost
+        sops-nix.nixosModules.sops
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.sharedModules = [ sops-nix.homeManagerModules.sops ];
+        }
+      ];
+    };
+
     ## local-proxy ##
     nixosConfigurations.local-proxy = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./hosts/dev/local-proxy
-sops-nix.nixosModules.sops
+        sops-nix.nixosModules.sops
 
         home-manager.nixosModules.home-manager
         {
