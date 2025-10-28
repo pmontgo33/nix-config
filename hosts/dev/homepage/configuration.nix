43d26db43df4d5882e2024c593db62b106b77add
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-10-28 11:57:28 -0400

    troubleshooting homepage

--- hosts/dev/homepage/configuration.nix
+++ hosts/dev/homepage/configuration.nix
@@ -25,7 +25,7 @@
 
   services.homepage-dashboard = {
     enable=true;
-    package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.homepage-dashboard;
+    # package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.homepage-dashboard;
     # allowedHosts = "homepage.montybeta.org,localhost:8082,127.0.0.1:8082,192.168.86.177:8082";
     allowedHosts = "*";
     environmentFile = config.sops.secrets."homepage-dashboard-env".path;
