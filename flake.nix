f75c80e83a21dec0dcdbb2a40fd224a498062d13
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-10-27 21:55:30 -0400

    added homepage to flake

--- flake.nix
+++ flake.nix
@@ -241,6 +241,25 @@
       ];
     };
 
+    ## homepage ##
+    nixosConfigurations.homepage = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/dev/homepage
+        sops-nix.nixosModules.sops
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.sharedModules = [ sops-nix.homeManagerModules.sops ];
+        }
+      ];
+    };
+
     ## endurain ##
     nixosConfigurations.endurain = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
