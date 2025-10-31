1a236d645862dc7d8b1a5384ac36aec89a9eb31e
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-10-30 20:13:06 -0400

    troubleshooting homepage

--- hosts/dev/homepage/configuration.nix
+++ hosts/dev/homepage/configuration.nix
@@ -304,34 +304,77 @@
         ];
       }
       {
-        Developer = [
+        "Cloud Storage" = [
           {
-            Github = [{
-              icon = "si-github";
-              href = "https://github.com/";
-            }];
+            "TrueCloud - Storj" = [
+              {
+                icon = "https://us1.storj.io/static/dist/assets/logo-dark-B-1o513O.svg";
+                href = "https://us1.storj.io/projects/KCrA-smpTX2/dashboard";
+              }
+            ];
           }
           {
-            "Nixos Search" = [{
-              icon = "si-nixos";
-              href = "https://search.nixos.org/packages";
-            }];
-          }
-          {
-            "Nixos Wiki" = [{
-              icon = "si-nixos";
-              href = "https://nixos.wiki/";
-            }];
-          }
-          {
-            "Kubernetes Docs" = [{
-              icon = "si-kubernetes";
-              href = "https://kubernetes.io/docs/home/";
-            }];
+            "Backblaze" = [
+              {
+                icon = "sh-backblaze.svg";
+                href = "https://secure.backblaze.com/user_signin.htm";
+              }
+            ];
           }
         ];
       }
-      
+      {
+        "Documentation" = [
+          {
+            "Home Assistant Docs" = [
+              {
+                icon = "sh-home-assistant.svg";
+                href = "https://www.home-assistant.io/docs/";
+              }
+            ];
+          }
+          {
+            "Homepage Docs" = [
+              {
+                icon = "sh-homepage.png";
+                href = "https://gethomepage.dev/";
+              }
+            ];
+          }
+          {
+            "Frigate Docs" = [
+              {
+                icon = "sh-frigate.svg";
+                href = "https://docs.frigate.video/";
+              }
+            ];
+          }
+          {
+            "NixOS Search" = [
+              {
+                icon = "sh-nixos.svg";
+                href = "https://search.nixos.org/packages";
+              }
+            ];
+          }
+          {
+            "NixOS Wiki" = [
+              {
+                icon = "sh-nixos.svg";
+                href = "https://nixos.wiki/wiki/Main_Page";
+              }
+            ];
+          }
+          {
+            "Ansible Docs" = [
+              {
+                icon = "sh-ansible.svg";
+                href = "https://docs.ansible.com/ansible/latest/index.html";
+              }
+            ];
+          }
+        ];
+      }
     ];
 
     services = [
