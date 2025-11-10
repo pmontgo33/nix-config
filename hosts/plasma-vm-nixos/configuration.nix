04e9fa9835e249d288113cda013c1fa1e71c5d84
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-11-10 15:28:31 -0500

    revert testing referencing hardware config from /etc/nixos

--- hosts/plasma-vm-nixos/configuration.nix
+++ hosts/plasma-vm-nixos/configuration.nix
@@ -7,8 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      # ./hardware-configuration.nix
-      /etc/nixos/hardware-configuration.nix
+      ./hardware-configuration.nix
       ../../users/patrick/hosts/hp-nixos
       ../../secrets
     ];
