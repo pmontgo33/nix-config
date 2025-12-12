8f66350a9c8bbec44db790ac6b9bde07f44b20d2
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-12-11 22:13:17 -0500

    added new host frigate as nxc

--- flake.nix
+++ flake.nix
@@ -515,5 +515,24 @@
         }
       ];
     };
+
+    ## frigate ##
+    nixosConfigurations.frigate = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/nxc/frigate
+        sops-nix.nixosModules.sops
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = { inherit inputs; };
+
+          home-manager.sharedModules = [ sops-nix.homeManagerModules.sops ];
+        }
+      ];
+    };
   };
 }
