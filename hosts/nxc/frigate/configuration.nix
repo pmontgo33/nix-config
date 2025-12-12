ca508c0b629cb13013a5f161c49243e5d3e188fd
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-12-12 11:03:54 -0500

    troubleshooting frigate

--- hosts/nxc/frigate/configuration.nix
+++ hosts/nxc/frigate/configuration.nix
@@ -457,6 +457,9 @@
       proxyPass = "http://127.0.0.1:5000";
       proxyWebsockets = true;
       extraConfig = ''
+        # Disable auth at proxy level - let backend handle it
+        auth_request off;
+
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
