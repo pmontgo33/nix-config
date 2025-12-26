8a6897673045f34df7adc59936d7f355218dc0ff
Monty <21371673+pmontgo33@users.noreply.github.com>
2025-12-26 15:16:41 -0500

    Update host verification dates and add home_media NFS mount
    - Updated host-states.md with latest verification dates (2025-12-26)
    - Updated rebuild dates for multiple hosts
    - Added new Omada host entry
    - Added Youtarr container to Yondu host
    - Configured home_media NFS mount on Tesseract with automount options
    - Added systemd tmpfiles rules to ensure mount directories exist

--- users/patrick/hosts/tesseract/default.nix
+++ users/patrick/hosts/tesseract/default.nix
@@ -25,6 +25,20 @@
     ];
   };
 
+  # Mount home_media NFS Share in home directory
+  fileSystems."/home/patrick/mnt/home_media" = {
+    device = "truenas:/mnt/HDD-Mirror-01/home_media";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "nofail"
+      "_netdev"
+      "x-systemd.idle-timeout=600"
+      "x-systemd.device-timeout=5"
+    ];
+  };
+
   # Mount nextcloud NFS Share in home directory
   fileSystems."/home/patrick/mnt/nextcloud" = {
     device = "truenas:/mnt/HDD-Mirror-01/drive/patrick/files";
@@ -38,6 +52,14 @@
       "x-systemd.device-timeout=5"
     ];
   };
+
+  # Create mount point directories if they don't exist
+  systemd.tmpfiles.rules = [
+    "d /home/patrick/mnt 0755 patrick users -"
+    "d /home/patrick/mnt/media 0755 patrick users -"
+    "d /home/patrick/mnt/home_media 0755 patrick users -"
+    "d /home/patrick/mnt/nextcloud 0755 patrick users -"
+  ];
 
   # optional, but ensures rpc-statsd is running for on demand mounting
   boot.supportedFilesystems = [ "nfs" ];
