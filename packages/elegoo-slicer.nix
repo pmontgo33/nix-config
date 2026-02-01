3c4addc35136f6448a9c4a87ef36ef1bb060d1fe
Monty <21371673+pmontgo33@users.noreply.github.com>
2026-02-01 13:32:09 -0500

    Simplify elegoo-slicer to bare minimum for home-manager

--- packages/elegoo-slicer.nix
+++ packages/elegoo-slicer.nix
@@ -1,9 +1,8 @@
-{ writeShellScriptBin
+{ stdenv
 , fetchurl
-, appimage-run
 }:
 
-let
+stdenv.mkDerivation rec {
   pname = "elegoo-slicer";
   version = "1.3.0.11";
 
@@ -12,12 +11,12 @@ let
     hash = "sha256-5c3cb581cc101598e8d581270405031edb1ca091e391925dba06c9dc7a7360c8";
   };
 
-  # Override appimage-run to include webkitgtk_4_1
-  appimage-run-webkit = appimage-run.override {
-    extraPkgs = pkgs: [ pkgs.webkitgtk_4_1 ];
-  };
-in
+  dontUnpack = true;
+  dontBuild = true;
 
-writeShellScriptBin pname ''
-  exec ${appimage-run-webkit}/bin/appimage-run ${src} "$@"
-''
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${src} $out/bin/elegoo-slicer
+    chmod +x $out/bin/elegoo-slicer
+  '';
+}
